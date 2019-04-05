e19cb5a667664290062559a3e57440bad7411e69
CBT-CGL-HAEM-QMH <49255975+QMH-HAEM@users.noreply.github.com>
2019-04-05 14:39:43 +0800

    Update and rename 01.Introduction_to_Basic_Linux_Commands to 01.Introduction_to_Basic_Linux_Commands.sh

---
+++ 01.Introduction_to_Basic_Linux_Commands.sh
@@ -0,0 +1,16 @@
+####################################################################################################
+# Introduction to basic Linux commands
+
+# This session cover Linux file structure and basic commands including:
+# - pwd
+# - cd
+# - mkdir
+# - rmdir
+# - mv
+# - cp
+# - touch
+# - cat
+# - less
+
+# Concepts of absolute and relative file paths are introduced
+#####################################################################################################
