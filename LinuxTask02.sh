fb0a39ac111f553c7bbead5a1472343413c10196
IP-lab <49301952+IP-lab@users.noreply.github.com>
2019-04-05 17:03:06 +0800

    Add files via upload

---
+++ LinuxTask02.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
+mkdir LinuxTask02
+cd LinuxTask02
+touch 19H0011234_S1_SNP_Haplo_FINAL.txt
+touch 19H0011234_S1_INDEL_Haplo_FINAL.txt
+touch 19H0055678_S2_SNP_Haplo_FINAL.txt
+touch 19H0055678_S2_INDEL_Haplo_FINAL.txt
+touch 19H0102345_S3_SNP_Haplo_FINAL.txt
+touch 19H0102345_S3_INDEL_Haplo_FINAL.txt
+touch 19H0203456_S4_SNP_Haplo_FINAL.txt
+touch 19H0203456_S4_INDEL_Haplo_FINAL.txt
+touch 19H0304567_S10_SNP_Haplo_FINAL.txt
+touch 19H0304567_S10_INDEL_Haplo_FINAL.txt
