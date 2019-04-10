a7b2710b370f7bdd69c1e31beeda9063d66ca7a3
CBT-CGL-HAEM-QMH <49255975+QMH-HAEM@users.noreply.github.com>
2019-04-11 07:22:16 +0800

    Update 04.Compile_List_of_Canonical_Transcripts.sh

--- 04.Compile_List_of_Canonical_Transcripts.sh
+++ 04.Compile_List_of_Canonical_Transcripts.sh
@@ -1,5 +1,5 @@
 # Compile a list of canonical transcripts for given genes
-# Download from UCSC:
+# Download from UCSC hg19 website at http://hgdownload.cse.ucsc.edu/goldenPath/hg19/database/
 # - kgXref.txt.gz
 # - knownCanonical.txt.gz
 # - knownToEnsembl.txt.gz
