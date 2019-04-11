48f61684f98477b57feebc4c3c461fb8aa583c86
CBT-CGL-HAEM-QMH <49255975+QMH-HAEM@users.noreply.github.com>
2019-04-11 22:31:06 +0800

    Update 04.Compile_List_of_Canonical_Transcripts.sh

--- 04.Compile_List_of_Canonical_Transcripts.sh
+++ 04.Compile_List_of_Canonical_Transcripts.sh
@@ -4,3 +4,40 @@
 # - knownCanonical.txt.gz
 # - knownToEnsembl.txt.gz
 
+history | grep sed
+history | grep ssh
+cat refGene.txt | cut -f13 | head
+cat refGene.txt | cut -f2 | head
+join --help
+cat testlist.txt
+cat refGene.txt | less -SN
+cat testlist.txt | sort -k 2,2
+cat testlist.txt | sort -k 2,2 > testlist.sort.txt
+cat testlist.sort.txt
+cat refGene.txt | sort -k 2,2 > refGene.sort.txt
+cat refGene.sort.txt | less -SN
+sort --help
+join -1 2 -2 2 testlist.sort.txt refGene.sort.txt | less -SN
+join -t $'\t' -1 2 -2 2 testlist.sort.txt refGene.sort.txt | less -SN
+cat refGene.txt | grep TP53 | column -t -s $'\t' | less -SN
+cat refGene.txt | grep TP53 | grep chr17 | column -t -s $'\t' | less -SN
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/knownCanonical.txt.gz
+gunzip  knownCanonical.txt.gz
+cat knownCanonical.txt | less -SN
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/kgXref.txt.gz
+gunzip kgXref.txt.gz
+ll -h
+cat kgXref.txt | less -SN
+cat kgXref.txt | head -1000 | column -t -s $'\t' | less -SN
+cat kgXref.txt | less -SN
+cat kgXref.txt | head -1000 | column -t -s $'\t' | less -SN
+cat kgXref.txt | grep TP53 | less -SN
+cat kgXref.txt | grep -P '\tTP53\t' | less -SN
+wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/knownToEnsembl.txt.gz
+gunzip knownToEnsembl.txt.gz
+cat knownToEnsembl.txt | less -SN
+cat knownCanonical.txt | less -SN
+cat kgXref.txt | less -SN
+cat kgXref.txt | head -1000 | column -t -s $'\t' | less -SN
+cat kgXref.txt | grep -P '\tTP53\t' | less -SN
+cat knownToEnsembl.txt | less -SN
